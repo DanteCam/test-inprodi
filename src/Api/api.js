479b5174a6397d92a6f9ac2506561ad023cdca3e
DanteCam <76830701+DanteCam@users.noreply.github.com>
2022-04-12 02:04:55 -0500

    improvements

--- src/Api/api.js
+++ src/Api/api.js
@@ -11,7 +11,7 @@ let LoginApi;
 if (process.env.NODE_ENV === "development") {
   LoginApi = "http://localhost:4000/";
 } else if (process.env.NODE_ENV === "production") {
-  LoginApi = "https://improdi-examen.herokuapp.com/"; //Change for prod deploy
+  LoginApi = "https://improd.herokuapp.com/"; //Change for prod deploy
 }
 
 export const loginApi = axios.create({
